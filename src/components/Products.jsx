c69dd1d45d7da4c0c6b764edd850d9eb1b94665b
admire Excel <godwillokenyi@gmail.com>
2023-03-18 14:58:53 +0100

    yes

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://mern-api-excel.onrender.com/api/products?category=${cat}`
+            : "https://mern-api-excel.onrender.com/api/products"
         );
         setProducts(res.data);
       } catch (err) {}
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://mern-api-excel.onrender.com/api/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
